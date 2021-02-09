7303c17afba0e1b704adae4d07dd5dc69b8898f6
FerretLeader <42744568+FerretLeader@users.noreply.github.com>
2021-02-09 17:43:44 -0600

    current time is now displayed

--- src/alarm/alarm.java
+++ src/alarm/alarm.java
@@ -1,6 +1,7 @@
 package alarm;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
@@ -10,6 +11,7 @@ public class alarm {
 		JButton startAlarm = new JButton();
 		JButton stopAlarm = new JButton();
 		ImageIcon icon = new ImageIcon("clock.png");
+		JLabel currentTime = new JLabel("00:00");
 		
 		//startAlarm stuff
 		startAlarm.setText("Start");
@@ -22,6 +24,9 @@ public class alarm {
 		stopAlarm.addActionListener(e -> stopTheAlarm(startAlarm, stopAlarm));
 		stopAlarm.setEnabled(false);
 		
+		//currentTime stuff
+		currentTime.setBounds(0, 0, 200, 100);
+		
 		//myWindow stuff
 		myWindow.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		myWindow.setResizable(false);
@@ -30,7 +35,11 @@ public class alarm {
 		myWindow.setIconImage(icon.getImage());
 		myWindow.add(startAlarm);
 		myWindow.add(stopAlarm);
+		myWindow.add(currentTime);
 		myWindow.setLayout(null);
+		
+		//method calls
+		displayTheTime(currentTime);
 	}
 	
 	static void startTheAlarm(JButton startAlarm, JButton stopAlarm){
@@ -41,5 +50,28 @@ public class alarm {
 	static void stopTheAlarm(JButton startAlarm, JButton stopAlarm){
 		startAlarm.setEnabled(true);
 		stopAlarm.setEnabled(false);
+	}
+	
+	static void displayTheTime(JLabel currentTime) {		
+		long milliseconds = 0;
+		long hours = 0;
+		long minutes = 0;
+		long seconds = 0;
+		
+		while(true) {
+			milliseconds = System.currentTimeMillis();
+			seconds = (milliseconds/1000)%60;
+			minutes = (milliseconds/60000)%60;
+			hours = (milliseconds/3600000)%24;
+			
+			currentTime.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+			
+			// wait a second to update
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
