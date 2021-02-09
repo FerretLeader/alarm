17e734b66c7606cdc10b37a342d9761c16e2e1f6
FerretLeader <42744568+FerretLeader@users.noreply.github.com>
2021-02-09 16:24:20 -0600

    Some work dont on the start and stop timer methods

--- src/alarm/alarm.java
+++ src/alarm/alarm.java
@@ -5,23 +5,41 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class alarm {
-	JButton setAlarm;
-	
 	public static void main(String[] args) {
 		JFrame myWindow = new JFrame("Alarm");
-		setAlarm = new JButton();
+		JButton startAlarm = new JButton();
+		JButton stopAlarm = new JButton();
 		ImageIcon icon = new ImageIcon("clock.png");
 		
-		setAlarm.setText("Start");
-		setAlarm.setBounds(350, 400, 100, 25);
-		setAlarm.addActionListener(e -> System.out.print("asdf\n"));
+		//startAlarm stuff
+		startAlarm.setText("Start");
+		startAlarm.setBounds(350, 400, 100, 25);
+		startAlarm.addActionListener(e -> startTheAlarm(startAlarm, stopAlarm));
 		
+		//stopAlarm stuff
+		stopAlarm.setText("Stop");
+		stopAlarm.setBounds(250, 400, 100, 25);
+		stopAlarm.addActionListener(e -> stopTheAlarm(startAlarm, stopAlarm));
+		stopAlarm.setEnabled(false);
+		
+		//myWindow stuff
 		myWindow.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		myWindow.setResizable(false);
 		myWindow.setSize(500, 500);
 		myWindow.setVisible(true);
 		myWindow.setIconImage(icon.getImage());
-		myWindow.add(setAlarm);
+		myWindow.add(startAlarm);
+		myWindow.add(stopAlarm);
 		myWindow.setLayout(null);
+	}
+	
+	static void startTheAlarm(JButton startAlarm, JButton stopAlarm){
+		startAlarm.setEnabled(false);
+		stopAlarm.setEnabled(true);
+	}
+	
+	static void stopTheAlarm(JButton startAlarm, JButton stopAlarm){
+		startAlarm.setEnabled(true);
+		stopAlarm.setEnabled(false);
 	}
 }
